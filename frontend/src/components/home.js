9a7be0a41bce3c2e1ce1bd15ddbe8c54c284bb3c
Santiago Garcia Saiz <santiago.garcia.saiz@gmail.com>
2021-01-25 22:02:40 -0300

    Version 1.3

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,12 +1,19 @@
+import background from "..//home_image.jpg";
 
-const Home = () =>{
-
-return(
-<div>
- <h5>Welcome, this is an income tax calculator. </h5>
- <h5>Please press 'Tax Calculator' menu to start procesing incomes</h5>
-
- </div>);
-
-}
+const Home = () => {
+  return (
+    <div>
+      <h5>Welcome, this is an income tax calculator. </h5>
+      <h5>Please press 'Tax Calculator' menu to start procesing incomes</h5>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          width: "1000px",
+          height: "1000px",
+        }}
+      ></div>
+    </div>
+  );
+};
 export default Home;
